69c1c1e8bf15457e178ca578fd8d7b208b681973
mehtananya <mehtananya136@gmail.com>
2024-11-04 16:21:57 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <p>Hi</p>
-        <p></p>
-        </div>
-    </>
-  )
+    <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
+  </Router>
+  );
 }
 
 
-
-
-export default App
+export default App;
